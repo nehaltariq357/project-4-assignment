7b965e548c2f5679d55b8580c127176a1f018bfd
nehaltariq357 <143392985+nehaltariq357@users.noreply.github.com>
2025-04-21 21:03:55 +0500

    update

---
+++ assignment-01/01_basics/01_double_it.py
@@ -0,0 +1,10 @@
+
+
+def main():
+    current_value = int(input("Enter a number: "))
+    while current_value < 100:
+        current_value = current_value * 2
+        print(current_value)
+
+if __name__ == '__main__':
+    main()
